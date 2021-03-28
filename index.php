d2200a4effaad9a61a76ed13a55f422512873fb2
kinoppol <kinoppol@hotmail.com>
2021-03-28 13:24:20 +0700

    darkmode

--- index.php
+++ index.php
@@ -33,19 +33,111 @@
         background-size: cover;
         background-position: center;
     }
-    h4{
-        color:navy;
+    .modal-backdrop {background-color:rgba(0, 0, 0, 0.4);}
+    .switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+.dark-mode {
+  background-image: url("bg-dark.jpg");
+  background-color: black;
+  color: white;
+}
+.dark-mode .card{
+      background-color:rgba(0, 0, 0, 0.8);
+    }
+.dark-mode h4{
+        color:red;
+    }
+.dark-mode p{
+        color:OrangeRed;
+    }
+.dark-mode a{
+        color:CornflowerBlue;
+        text-decoration:none; 
+    }
+.dark-mode a:hover{
+        color:White; 
+        text-decoration:underline; 
     }
     </style>
     <link rel="icon" href="favicon.ico"  type="image/icon type">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 </head>
-<body data-menu="vertical-menu" class="vertical-layout vertical-menu 2-columns container boxed-layout menu-collapsed">
+<body data-menu="vertical-menu" class="vertical-layout vertical-menu 2-columns container boxed-layout menu-collapsed" style="display: none;">
 <div class="container-fluid">
 	<section id="minimal-statistics">
         <div class="row">
-          <div class="col-12 mt-3 mb-1">
+          <div class="col-8 mt-3 mb-1">
             <h4 class="text-uppercase">วิทยาลัยพณิชยการบางนา : Bangna Commercial College.</h4>
             <p>อาชีวศึกษากรุงเทพมหานคร : Institute of Vocational Education, Bangkok.</p>
+          </div> 
+          <div class="col-4 mt-3 mb-1" style="text-align:center">
+          <p>โหมดกลางคืน</p>
+          <label class="switch">
+            <input id="darkModeSwitch" type="checkbox">
+            <span class="slider round" onClick="darkMode()"></span>
+          </label>
+          </div>
           </div>
         </div>
         <div class="row">
@@ -70,8 +162,8 @@
         </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://www.bncc.ac.th/web/category/ข่าวประชาสัมพันธ์/">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -85,12 +177,12 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://www.bncc.ac.th/web/ข้อมูลพื้นฐานสถานศึกษา/">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -104,12 +196,12 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://www.bncc.ac.th/web/ติดต่อเรา/">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -123,15 +215,15 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
         </div>
       
         <div class="row">
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://www.bncc.ac.th/web/category/ภาพกิจกรรม/">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -145,12 +237,12 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://rms.bncc.ac.th">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -164,13 +256,13 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
       
           <div class="col-xl-3 col-sm-6 col-12">
+                <a href="#deptBox" data-toggle="modal" data-target="#deptBox">
             <div class="card">
-                <a href="https://rms.bncc.ac.th">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -184,12 +276,12 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
+                <a href="#deptBox" data-toggle="modal" data-target="#secBox">
             <div class="card">
-                <a href="https://rms.bncc.ac.th">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -203,15 +295,15 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
           </div>
+              </a>
         </div>
       
         <div class="row">
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://rms.bncc.ac.th">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -225,12 +317,12 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="https://std2018.vec.go.th">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -244,13 +336,13 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
       
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">                
                 <a href="https://print.bncc.ac.th">
+            <div class="card">                
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -264,12 +356,12 @@
                   </div>
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           <div class="col-xl-3 col-sm-6 col-12">
-            <div class="card">
                 <a href="http://web.bncc.ac.th/site/">
+            <div class="card">
               <div class="card-content">
                 <div class="card-body">
                   <div class="media d-flex">
@@ -287,8 +379,8 @@
                   </div>-->
                 </div>
               </div>
-              </a>
             </div>
+              </a>
           </div>
           
         </div>
@@ -299,8 +391,8 @@
     <section id="minimal-statistics">
       <div class="row d-flex justify-content-center ">
         <div class="col-xl-3 col-sm-6 col-12"> 
-          <div class="card">
               <a href="https://www.bncc.ac.th/web/">
+          <div class="card">
             <div class="card-content">
               <div class="card-body">
                 <div class="media d-flex">
@@ -314,18 +406,85 @@
                 </div>
               </div>
             </div>
-            </a>
           </div>
+            </a>
         </div>
       </div>
     </section>
 </div>
 <footer class="text-center text-lg-start">
 <div class="text-center p-3">
-    © <?php print date('Y'); ?> Copyright: <a class="text-dark" href="https://bncc.ac.th/">www.bncc.ac.th</a><br>พัฒนาและดูแลระบบโดย งานศูนย์ข้อมูลสารสนเทศ ฝ่ายแผนงานและความร่วมมือ วิทยาลัยพณิชยการบางนา
+    © <?php print date('Y'); ?> Copyright: <a href="https://bncc.ac.th/">www.bncc.ac.th</a><br>พัฒนาและดูแลระบบโดย งานศูนย์ข้อมูลสารสนเทศ ฝ่ายแผนงานและความร่วมมือ วิทยาลัยพณิชยการบางนา
     
   </div>
   <!-- Copyright -->
 </footer>
+<div class="modal" tabindex="-1" role="dialog" id="deptBox" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">แผนกวิชา</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="d-grid gap-2">
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาการบัญชี</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาการตลาด</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาการจัดการเลขานุการ</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาคอมพิวเตอร์ธุรกิจ</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาภาษาต่างประเทศ</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาการโรงแรม</a>        
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาการจัดประชุมและนิทรรศการ</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาการจัดการโลจิสติกส์</a>
+        <a href="#" class="btn btn-primary btn-block">แผนกวิชาเทคโนโลยีสารสนเทศ</a>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal" tabindex="-1" role="dialog" id="secBox" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">หน่วยงานภายใน</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="d-grid gap-2">
+        <a href="#" class="btn btn-primary btn-block">ฝ่ายพัฒนากิจการนักเรียนนักศึกษา</a>
+        <a href="#" class="btn btn-primary btn-block">ฝ่ายแผนงานและความร่วมมือ</a>
+        <a href="#" class="btn btn-primary btn-block">ฝ่ายวิชาการ</a>
+        <a href="#" class="btn btn-primary btn-block">ฝ่ายบริหารทรัพยากร</a>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+$(function(){
+  var d = new Date();
+  if(d.getHours()>17||d.getHours()<6){
+    $("#darkModeSwitch").prop('checked', true);
+      darkMode();
+  }
+  $('html, body').show()
+});
+function darkMode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
 <body>
 </html>
